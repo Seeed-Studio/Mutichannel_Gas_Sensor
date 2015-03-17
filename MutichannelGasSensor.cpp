65b9ac568d3b28a2583680b4c0048f2f338312ac
Jacky Zhang <qi.zhang@Seeed.cc>
2015-03-17 17:14:37 +0800

    upload code and license files

---
+++ MutichannelGasSensor.cpp
@@ -0,0 +1,187 @@
+/*
+    MutichannelGasSensor.cpp
+    2015 Copyright (c) Seeed Technology Inc.  All right reserved.
+
+    Author: Jacky Zhang
+    2015-3-17
+    http://www.seeed.cc/
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <Wire.h>
+#include <Arduino.h>
+#include "MutichannelGasSensor.h"
+
+/*********************************************************************************************************
+** Function name:           begin
+** Descriptions:            initialize I2C
+*********************************************************************************************************/
+void MutichannelGasSensor::begin(int address)
+{
+    Wire.begin();
+    i2cAddress = address;
+}
+
+/*********************************************************************************************************
+** Function name:           sendI2C
+** Descriptions:            send one byte to I2C Wire
+*********************************************************************************************************/
+void MutichannelGasSensor::sendI2C(unsigned char dta)
+{
+    Wire.beginTransmission(i2cAddress);               // transmit to device #4
+    Wire.write(dta);                                    // sends one byte
+    Wire.endTransmission();                             // stop transmitting
+}
+
+/*********************************************************************************************************
+** Function name:           readData
+** Descriptions:            read 4 bytes from I2C slave
+*********************************************************************************************************/
+int16_t MutichannelGasSensor::readData(uint8_t cmd)
+{
+    uint16_t timeout = 0;
+    uint8_t buffer[4];
+    uint8_t checksum = 0;
+    int16_t rtnData = 0;
+
+    //send command
+    sendI2C(cmd);
+    //wait for a while
+    delay(2);
+    //get response
+    Wire.requestFrom(i2cAddress, 4);    // request 4 bytes from slave device
+    while(Wire.available() == 0)
+    {
+        if(timeout++ > 500)
+            return -2;//time out
+        delay(2);
+    }
+    if(Wire.available() != 4)
+        return -3;//rtnData length wrong
+    buffer[0] = Wire.read();
+    buffer[1] = Wire.read();
+    buffer[2] = Wire.read();
+    buffer[3] = Wire.read();
+    checksum = (uint8_t)(buffer[0] + buffer[1] + buffer[2]);
+    if(checksum != buffer[3])
+        return -4;//checksum wrong
+    rtnData = ((buffer[1] << 8) + buffer[2]);
+    
+    return rtnData;//successful
+}
+
+/*********************************************************************************************************
+** Function name:           readR0
+** Descriptions:            read R0 stored in slave MCU
+*********************************************************************************************************/
+int16_t MutichannelGasSensor::readR0(void)
+{
+    int16_t rtnData = 0;
+
+    rtnData = readData(0x11);
+    if(rtnData >= 0)
+        res0[0] = rtnData;
+    else
+        return -1;//unsuccessful
+
+    rtnData = readData(0x12);
+    if(rtnData >= 0)
+        res0[1] = rtnData;
+    else
+        return -1;//unsuccessful
+
+    rtnData = readData(0x13);
+    if(rtnData >= 0)
+        res0[2] = rtnData;
+    else
+        return -1;//unsuccessful
+
+    return 0;//successful
+}
+
+/*********************************************************************************************************
+** Function name:           readR
+** Descriptions:            read resistance value of every channel from slave MCU
+*********************************************************************************************************/
+int16_t MutichannelGasSensor::readR(void)
+{
+    int16_t rtnData = 0;
+
+    rtnData = readData(0x01);
+    if(rtnData >= 0)
+        res[0] = rtnData;
+    else
+        return -1;//unsuccessful
+
+    rtnData = readData(0x02);
+    if(rtnData >= 0)
+        res[1] = rtnData;
+    else
+        return -1;//unsuccessful
+
+    rtnData = readData(0x03);
+    if(rtnData >= 0)
+        res[2] = rtnData;
+    else
+        return -1;//unsuccessful
+
+    return 0;//successful
+}
+
+/*********************************************************************************************************
+** Function name:           changeI2cAddr
+** Descriptions:            change I2C address of the slave MCU, and this address will be stored in EEPROM of slave MCU
+*********************************************************************************************************/
+void MutichannelGasSensor::changeI2cAddr(uint8_t newAddr)
+{
+    Wire.beginTransmission(i2cAddress); // transmit to device
+    Wire.write(0x23);              // sends one byte
+    Wire.write(newAddr);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    i2cAddress = newAddr;
+}
+
+/*********************************************************************************************************
+** Function name:           doCalibrate
+** Descriptions:            tell slave to do a calibration, it will take about 8~10s
+*********************************************************************************************************/
+void MutichannelGasSensor::doCalibrate(void)
+{
+    sendI2C(0x22);
+}
+
+/*********************************************************************************************************
+** Function name:           powerOn
+** Descriptions:            power on sensor heater
+*********************************************************************************************************/
+void MutichannelGasSensor::powerOn(void)
+{
+    sendI2C(0x21);
+}
+
+/*********************************************************************************************************
+** Function name:           powerOff
+** Descriptions:            power off sensor heater
+*********************************************************************************************************/
+void MutichannelGasSensor::powerOff(void)
+{
+    sendI2C(0x20);
+}
+
+MutichannelGasSensor mutichannelGasSensor;
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
---
+++ MutichannelGasSensor.h
@@ -0,0 +1,56 @@
+/*
+    MutichannelGasSensor.h
+    2015 Copyright (c) Seeed Technology Inc.  All right reserved.
+
+    Author: Jacky Zhang
+    2015-3-17
+    http://www.seeed.cc/
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef __MUTICHANNELGASSENSOR_H__
+#define __MUTICHANNELGASSENSOR_H__
+
+class MutichannelGasSensor{
+
+private:
+
+    uint8_t i2cAddress;//I2C address of this MCU
+    
+    void sendI2C(unsigned char dta);
+    int16_t readData(uint8_t cmd);
+
+public:
+
+    uint16_t res0[3] = {0, 0, 0};//sensors res0
+    uint16_t res[3] = {0, 0, 0};//sensors res
+    
+    void begin(int address);
+    int16_t readR0(void);
+    int16_t readR(void);
+    void changeI2cAddr(uint8_t newAddr);
+    void doCalibrate(void);
+    void powerOn(void);
+    void powerOff(void);
+};
+
+extern MutichannelGasSensor mutichannelGasSensor;
+
+#endif
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
